414364789adbe017538d311d215f5bbeaf538e86
hlzhou0124 <zhl_nuist@163.com>
2021-07-15 22:38:30 +0800

    istio访问权限逻辑调整bug

--- k8s/AuthorizationPolicy.py
+++ k8s/AuthorizationPolicy.py
@@ -93,7 +93,7 @@ class AuthorizationPolicy:
         self.logger.info("开始创建AuthorizationPolicy.yaml")
         self.logger.info("AuthorizationPolicy配置如下：")
         self.logger.info(policy_info)
-        policy_yaml_j2 = '%s/templates/policy/namespace-authorizationpolicy.yaml.j2' % sys.path[0]
+        policy_yaml_j2 = '%s/templates/policy/service-default-authorizationpolicy.tyaml.j2' % sys.path[0]
         policy_yaml = '%s/authorizationPolicy.yaml' % self.policy_path
 
         code = j2_to_file("server", policy_info, policy_yaml_j2, policy_yaml)
